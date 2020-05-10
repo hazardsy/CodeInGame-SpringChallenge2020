4e11d9930921661f017a727efcad8f23a3307cd3
Sylvain Hazard <hazard.sylvain@gmail.com>
2020-05-10 20:06:43 +0200

    Always speed up

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import sys
 import time
 from typing import Any, Dict, List, Optional, Tuple
 
-# TODO : If sped up, compute the best two cells move (sum of cells ?)
 # TODO : Diffuse from previous as a way to get out of dead ends ?
+# TODO : Diffuse a base value based on last time a cell was visible
 # TODO : Diffuse big pellets on n_iter depending on map size
 # TODO : Increase diffuse range if pellets are in vision but every score is 0
 # TODO : Ajust diffusing numbers
@@ -40,8 +40,8 @@ def get_neighbors(
 
 
 def get_action(pac: Dict) -> str:
-    # if pac.get("cooldown", 1) < 1:
-    #    return f"SPEED {pac.get('id')}"
+    if pac.get("cooldown", 1) < 1:
+        return f"SPEED {pac.get('id')}"
     return f"MOVE {pac.get('id')} {pac.get('target', {}).get('x')} {pac.get('target', {}).get('y')}"
 
 
@@ -106,13 +106,6 @@ while True:
     visible_pac_count = int(input())  # all your pacs and enemy pacs in sight
     pacs: List[Dict] = []
     for i in range(visible_pac_count):
-        # pac_id: pac number (unique within a team)
-        # mine: true if this pac is yours
-        # x: position in the grid
-        # y: position in the grid
-        # type_id: unused in wood leagues
-        # speed_turns_left: unused in wood leagues
-        # ability_cooldown: unused in wood leagues
         (id, mine, x, y, type_id, speed_turns_left, ability_cooldown,) = input().split()
 
         pacs.append(
@@ -122,6 +115,7 @@ while True:
                 "mine": mine != "0",
                 "shape": type_id,
                 "cooldown": int(ability_cooldown),
+                "speed_turns_left": int(speed_turns_left),
             }
         )
 
@@ -129,27 +123,23 @@ while True:
 
     # Diffuse pellet values
     for i in range(visible_pellet_count):
-        # value: amount of points this pellet is worth
         int_x, int_y, value = [int(j) for j in input().split()]
         current_turn_cells = diffuse(current_turn_cells, (int_x, int_y), value, 0.5,)
-
-    # for coords, cell in current_turn_cells.items():
-    #     log_err(f"{coords} : {cell.get('value')}")
 
     action: str = ""
     for pac in [p for p in pacs if p.get("mine")]:
         current_pac_cells: Dict = copy.deepcopy(current_turn_cells)
-        print(pac, file=sys.stderr)
 
-        # Diffuse pacs values
+        # TODO : Refactor using a single loop and a method for getting diffuse value
+        # Diffuse friendly pacs values
         for friend_pac in [
             p for p in pacs if p.get("mine") and p.get("id") != pac.get("id")
         ]:
-            print(f"Diffusing friend : {friend_pac.get('id')}", file=sys.stderr)
             current_pac_cells = diffuse(
                 current_pac_cells, friend_pac.get("coordinates", (0, 0)), -10, 0.75,
             )
 
+        # Diffuse foe pacs values
         for foe_pac in [p for p in pacs if not p.get("mine")]:
             current_pac_cells = diffuse(
                 current_pac_cells,
@@ -161,6 +151,7 @@ while True:
                 max_iter=3,
             )
 
+        # First order neighbors
         neighbors: List[Dict] = [
             current_pac_cells.get(coords, {})
             for coords in get_neighbors(
@@ -168,6 +159,20 @@ while True:
             )
             if current_pac_cells.get(coords)
         ]
+
+        # Pac is speeding, get the best neighbor's neighbor
+        if pac.get("speed_turns_left", 0) > 0:
+            neighbors = [
+                current_pac_cells.get(coords, {})
+                for neighbor in neighbors
+                for coords in get_neighbors(
+                    (neighbor.get("x", 0), neighbor.get("y", 0)),
+                    width=width,
+                    height=height,
+                )
+                if current_pac_cells.get(coords)
+                and coords != pac.get("coordinates", (0, 0))
+            ]
 
         best_cells = sorted(neighbors, key=lambda x: x.get("value"), reverse=True)
         pac["target"] = best_cells[0]
